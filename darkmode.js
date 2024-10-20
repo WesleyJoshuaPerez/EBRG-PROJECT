71f3a0d5cf061e191929dc1412ee23ae44b39c5b
WesleyJoshuaPerez <122728678+WesleyJoshuaPerez@users.noreply.github.com>
2024-10-20 11:27:53 +0800

    dark mode to th system and some functions

--- darkmode.js
+++ darkmode.js
@@ -1,3 +1,21 @@
-document.getElementById('darkModeToggle').addEventListener('change', function () {
+// Dark mode toggle function
+function toggleDarkMode() {
     document.body.classList.toggle('dark-mode');
+
+    //Use to  change the text inside the link base on the current mode
+const darkModeLink = document.getElementById('darkModeLink');
+const logo = document.querySelector('img[alt="EBRGY logo"]'); //use to change the logo  image
+if (document.body.classList.contains('dark-mode')) {
+    darkModeLink.textContent = 'Light Mode'; // Switch to light mode text
+    logo.src = 'logo/logo_dark.png';
+} else {
+    darkModeLink.textContent = 'Dark Mode'; // Switch to dark mode text
+    logo.src = 'logo/mainpage_logo.png';
+}
+}
+
+// Add click event listener to the dark mode link
+document.getElementById('darkModeLink').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default link behavior
+    toggleDarkMode();
 });
